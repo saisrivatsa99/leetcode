a9156bd0a5ce85ef3a1b90c2f80b1695a48013c1
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-05-07 09:05:56 -0700

    Time: 62 ms (21.11%) | Memory: 44.9 MB (91.83%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize !=0) return false;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int h : hand) map.put(h, map.getOrDefault(h,0)+1);
+
+        while(map.size()>0){
+            int val = map.firstKey();
+            for(int i = val; i<val+groupSize; i++){
+                if(!map.containsKey(i)) return false;
+                map.put(i, map.get(i)-1);
+                if(map.get(i)==0) map.remove(i);
+            }
+        }
+        return true;
+    }
+}
