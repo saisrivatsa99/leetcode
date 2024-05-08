31ef99d4c7fe384e3c395af900ead161c5405410
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-05-07 18:37:22 -0700

    Time: 9 ms (65.84%) | Memory: 45.9 MB (17.05%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,29 +1,31 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        HashMap<Integer,Integer> m = new HashMap<>();
-        PriorityQueue<Integer> p = new PriorityQueue(Collections.reverseOrder());
-        int rank=0;
-        String[] ans = new String[score.length];
-        for(int i=0; i<score.length; i++){
-            m.put(score[i], i);
-            p.add(score[i]);
+            int[][] scoreWithIndex = new int[score.length][2];
+        for (int i = 0; i < score.length; i++) {
+            scoreWithIndex[i][0] = score[i]; // The score
+            scoreWithIndex[i][1] = i; // The original index
         }
-        while(p.size()!=0){
-            rank++;
-            int s = p.remove();
-            if(rank ==1){
-                ans[m.get(s)] = "Gold Medal";
+
+        // Sort the array in descending order based on the score
+        Arrays.sort(scoreWithIndex, (a, b) -> b[0] - a[0]);
+
+        // Initialize the result array
+        String[] res = new String[score.length];
+
+        // Assign ranks based on sorted order
+        for (int i = 0; i < scoreWithIndex.length; i++) {
+            int idx = scoreWithIndex[i][1]; // Original index
+            if (i == 0) {
+                res[idx] = "Gold Medal";
+            } else if (i == 1) {
+                res[idx] = "Silver Medal";
+            } else if (i == 2) {
+                res[idx] = "Bronze Medal";
+            } else {
+                res[idx] = String.valueOf(i + 1);
             }
-            else if(rank ==2){
-                ans[m.get(s)] = "Silver Medal";
-            }
-            else if(rank ==3){
-                ans[m.get(s)] = "Bronze Medal";
-            }
-            else{
-                ans[m.get(s)] = Integer.toString(rank);
-            }
-        } 
-        return ans;
+        }
+
+        return res;
     }
 }
