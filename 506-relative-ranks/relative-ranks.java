e08633eed1456da2be46350ebf5cbfc744b8af86
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-05-07 18:38:42 -0700

    Time: 6 ms (96.02%) | Memory: 45.6 MB (42.85%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,31 +1,33 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-            int[][] scoreWithIndex = new int[score.length][2];
-        for (int i = 0; i < score.length; i++) {
-            scoreWithIndex[i][0] = score[i]; // The score
-            scoreWithIndex[i][1] = i; // The original index
+        
+        int n = score.length;
+        String[] res = new String[n];
+        
+        PriorityQueue<Integer> pq = 
+            new PriorityQueue<>((a,b)->score[b]-score[a]);
+        
+        for(int i=0;i<n;i++){
+            pq.add(i);
         }
-
-        // Sort the array in descending order based on the score
-        Arrays.sort(scoreWithIndex, (a, b) -> b[0] - a[0]);
-
-        // Initialize the result array
-        String[] res = new String[score.length];
-
-        // Assign ranks based on sorted order
-        for (int i = 0; i < scoreWithIndex.length; i++) {
-            int idx = scoreWithIndex[i][1]; // Original index
-            if (i == 0) {
+        int i=1;
+        while(!pq.isEmpty()){
+            
+            int idx = pq.poll();
+            
+            if(i>3){
+                res[idx] = Integer.toString(i);
+            }else if(i==1){
                 res[idx] = "Gold Medal";
-            } else if (i == 1) {
+            }else if(i==2){
                 res[idx] = "Silver Medal";
-            } else if (i == 2) {
+            }else if(i==3){
                 res[idx] = "Bronze Medal";
-            } else {
-                res[idx] = String.valueOf(i + 1);
             }
+            i++;
         }
-
+        
         return res;
+        
     }
 }
