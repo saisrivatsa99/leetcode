7d6d337e7564f18344f64f5a69c6d86fdcf4d861
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-05-08 13:48:31 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (15.36%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,4 +1,5 @@
 class Solution {
+    /*
     public int climbStairs(int n) {
         //Top Down DP - Recursiion + Memoization
         Map<Integer, Integer> memo = new HashMap<>();
@@ -11,5 +12,19 @@ class Solution {
         if(memo.containsKey(n)) return memo.get(n);
         memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
         return memo.get(n);
+    }*/
+
+    //Bottom up DP
+    public int climbStairs(int n){
+        if(n<=1) return 1;
+
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
