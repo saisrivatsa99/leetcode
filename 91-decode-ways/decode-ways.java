9596b6552e2e7890f9d768c1efee8ef8976b664b
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-05-12 15:02:15 -0700

    Time: 0 ms (100.00%) | Memory: 41.3 MB (93.05%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,23 +1,25 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = s.charAt(0) != '0' ? 1 : 0;
-        for (int i = 2; i <= n; i++) {
-            int first = Integer.valueOf(s.substring(i - 1, i));
-            int second = Integer.valueOf(s.substring(i - 2, i));
-            if (first >= 1 && first <= 9) {
-               dp[i] += dp[i-1];  
-            }
-            if (second >= 10 && second <= 26) {
-                dp[i] += dp[i-2];
-            }
-        }
-        return dp[n];
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return decode(s,0,dp);
     }
+    public int decode(String s,int i,int[]dp){
+        if(i==s.length()){
+            return 1;
+        }
+        if(s.charAt(i)=='0'){
+            return 0; 
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        int ways = decode(s, i + 1,dp);
 
+        if (i < s.length() - 1 && (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i + 1) <= '6'))) {
+            ways += decode(s, i + 2,dp);
+        }
+        dp[i] = ways;
+        return ways;
+    }
 }
