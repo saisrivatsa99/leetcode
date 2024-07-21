42b4d2955ca5d5026b57d5d9cd261bd920caa361
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-07-21 14:58:40 -0400

    Time: 26 ms (99.27%) | Memory: 46.3 MB (44.39%) - LeetSync

--- 1905-design-authentication-manager/design-authentication-manager.java
+++ 1905-design-authentication-manager/design-authentication-manager.java
@@ -1,31 +1,67 @@
-class AuthenticationManager {
-    HashMap<String, Integer> map;
-    int time;
-    public AuthenticationManager(int timeToLive) {
-        map = new HashMap<>();
-        time = timeToLive;
-    }
-    
-    public void generate(String tokenId, int currentTime) {
-        map.put(tokenId, currentTime+time);
-    }
-    
-    public void renew(String tokenId, int currentTime) {
-        int expires = map.getOrDefault(tokenId,0);
-        if(expires> currentTime) map.put(tokenId, currentTime+time);
-    }
-    
-    public int countUnexpiredTokens(int currentTime) {
-        int count=0;
-        for(String key : map.keySet()) if(map.get(key) >currentTime) count++;
-        return count;        
+class Node {
+    Node prev;
+    Node next;
+    String tokenId;
+    int expiry;
+
+    Node(String tokenId,int expiry) {
+        this.tokenId = tokenId;
+        this.expiry = expiry;
     }
 }
 
-/**
- * Your AuthenticationManager object will be instantiated and called as such:
- * AuthenticationManager obj = new AuthenticationManager(timeToLive);
- * obj.generate(tokenId,currentTime);
- * obj.renew(tokenId,currentTime);
- * int param_3 = obj.countUnexpiredTokens(currentTime);
- */
+class AuthenticationManager {
+
+    Node head;
+    Node tail;
+    int timeToLive;
+    HashMap<String,Node> hmap;
+
+    public AuthenticationManager(int timeToLive) {
+        head = new Node("",-1);
+        tail = new Node("",-1);
+        head.next = tail;
+        tail.prev = head;
+        this.timeToLive = timeToLive;
+        hmap = new HashMap<>();
+    }
+
+    public void generate(String tokenId, int currentTime) {
+        Node node = new Node(tokenId,currentTime + timeToLive);
+
+        Node last = tail.prev;
+        last.next = node;
+        node.prev = last;
+        node.next = tail;
+        tail.prev = node;
+        hmap.put(tokenId,node);
+    }
+
+    public void renew(String tokenId, int currentTime) {
+        if(hmap.containsKey(tokenId) && hmap.get(tokenId).expiry>currentTime) {
+            Node node = hmap.get(tokenId);
+            node.expiry = currentTime+timeToLive;
+            Node prev = node.prev;
+            Node next = node.next;
+
+            prev.next = next;
+            next.prev = prev;
+
+            Node last = tail.prev;
+            last.next = node;
+            node.prev = last;
+            node.next = tail;
+            tail.prev = node;   
+        }
+    }
+
+    public int countUnexpiredTokens(int currentTime) {
+        while(head.next.expiry>0 && head.next.expiry<=currentTime) {
+            Node node = head.next;
+            hmap.remove(node.tokenId);
+            head.next = node.next;
+            node.next.prev = head;
+        }
+        return hmap.size();
+    }
+}
