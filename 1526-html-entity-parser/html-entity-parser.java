2c4703bbb6d84003a0a2470d2203540c569835e2
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-07-28 22:12:23 -0400

    Time: 24 ms (57.32%) | Memory: 45.3 MB (70.73%) - LeetSync

--- 1526-html-entity-parser/html-entity-parser.java
+++ 1526-html-entity-parser/html-entity-parser.java
@@ -1,14 +1,42 @@
-// Solution by Arman Nurniyazov
-
 class Solution {
     public String entityParser(String text) {
-        text = text.replace("&gt;", ">");
-        text = text.replace("&quot;", "\"");
-        text = text.replace("&apos;", "'");
-        text = text.replace("&lt;", "<");
-        text = text.replace("&frasl;", "/");
-        text = text.replace("&amp;", "&");
-        
-        return text;
+        Map<String, Character> map = init_map();
+        StringBuilder str = new StringBuilder();
+        int i=0, j=0;
+        while(i<text.length()){
+            if(text.charAt(i)=='&'){
+                StringBuilder s = new StringBuilder();
+                j=i+1;
+                while(j<text.length() && text.charAt(j)!=';') {
+                    s.append(text.charAt(j));
+                    j++;
+                }
+                //if(j<text.length() && text.charAt(j)!='&'){
+                    char c = map.getOrDefault(s.toString(), '+');
+                    if(c!='+') {
+                        str.append(c);
+                        i=j+1;
+                        continue;
+                    }
+                //}
+                str.append(text.charAt(i));
+                i++;
+            }else{
+                str.append(text.charAt(i));
+                i++;
+            }
+        }
+        return str.toString();
+    }
+
+    public Map<String, Character> init_map() {
+        Map<String, Character> map = new HashMap<String, Character>();
+        map.put("quot", '\"');
+        map.put("apos", '\'');
+        map.put("amp", '&');
+        map.put("gt", '>');
+        map.put("lt", '<');
+        map.put("frasl", '/');
+        return map;
     }
 }
