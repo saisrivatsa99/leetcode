9e6265a7776b4a9fb79aea240a9bf58ece108add
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-07-28 22:09:47 -0400

    Time: 20 ms (95.73%) | Memory: 45.5 MB (43.29%) - LeetSync

---
+++ 1526-html-entity-parser/html-entity-parser.java
@@ -0,0 +1,14 @@
+// Solution by Arman Nurniyazov
+
+class Solution {
+    public String entityParser(String text) {
+        text = text.replace("&gt;", ">");
+        text = text.replace("&quot;", "\"");
+        text = text.replace("&apos;", "'");
+        text = text.replace("&lt;", "<");
+        text = text.replace("&frasl;", "/");
+        text = text.replace("&amp;", "&");
+        
+        return text;
+    }
+}
