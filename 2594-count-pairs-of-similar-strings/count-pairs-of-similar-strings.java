966e3f5d6cc7df3154cd4357e98a4f14a44b8f20
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-07-28 22:17:12 -0400

    Time: 8 ms (77.79%) | Memory: 44.9 MB (31.03%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
@@ -1,25 +1,28 @@
 class Solution {
     public int similarPairs(String[] words) {
-        int count=0;
-        for(int i=0;i<words.length-1;i++)
-        {
-            for(int j=i+1;j<words.length;j++)
-            {
-                count+=check(words[i],words[j]);
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String word : words) {
+            int[] freq = new int[26];
+            for (char c : word.toCharArray()) {
+                int index = c - 'a';
+                if (freq[index] == 0)
+                    freq[index] = 1;
             }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i<26; i++) {
+                if (freq[i] == 1) 
+                    sb.append(i);
+            }
+            String s = sb.toString();
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        int count = 0;
+        for (String key : map.keySet()) {
+            if (map.get(key) >= 2)
+                count += map.get(key) * (map.get(key) - 1) / 2;
         }
         return count;
-    }
-
-    private int check(String a,String b)
-    {
-        HashSet<Character> aset=new HashSet();
-        for(char ch:a.toCharArray())
-            aset.add(ch);
-
-        HashSet<Character> bset=new HashSet();
-        for(char ch:b.toCharArray())
-            bset.add(ch);
-        return aset.equals(bset)?1:0;
     }
 }
