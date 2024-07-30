1cde2c3ad192eb6fbf755a00a920ef92842f0e8a
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-07-30 00:17:02 -0400

    Time: 2 ms (92.31%) | Memory: 57.5 MB (68.13%) - LeetSync

--- 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.java
+++ 3244-minimize-length-of-array-using-operations/minimize-length-of-array-using-operations.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumArrayLength(int[] nums) {
         int min=nums[0];
         for(int i=0;i<nums.length;i++)
-        min=Math.min(min,nums[i]);
+            min=Math.min(min,nums[i]);
         for(int n:nums){
             if(n%min>0)
             return 1;
