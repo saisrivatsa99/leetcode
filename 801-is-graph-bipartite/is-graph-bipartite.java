1e9b341d7c6e10513bbefd8bfe6a3a2e13f3a0a0
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-08-12 23:06:38 -0400

    Time: 0 ms (100.00%) | Memory: 44.8 MB (77.36%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,5 +1,5 @@
 class Solution {
-
+    /* BFS code
     public boolean bfs(int[][] graph, int[] check, int start){
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
@@ -26,6 +26,30 @@ class Solution {
         for(int i=0; i<graph.length; i++){
             if(check[i]==-1){
                 if(!bfs(graph,check, i)) return false;
+            }
+        }
+        return true;
+    }
+    */
+
+    public boolean dfs(int[][] graph, int[] check, int start, int color){
+        check[start] = color;
+        for(int i : graph[start]){
+            if(check[i]==-1){
+                if(!dfs(graph, check, i, 1-color)) return false;
+            }
+            else if(check[i]==color) return false;
+        }
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int[] check = new int[graph.length];
+        Arrays.fill(check, -1);
+
+        for(int i=0; i<graph.length; i++){
+            if(check[i]==-1){
+                if(!dfs(graph,check, i, 0)) return false;
             }
         }
         return true;
