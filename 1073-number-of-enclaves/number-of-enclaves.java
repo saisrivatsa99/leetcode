cfe99d3979e04a2d606361bcca914cc9fd6a4eac
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-08-12 21:55:48 -0400

    Time: 4 ms (100.00%) | Memory: 57.2 MB (47.96%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,56 +1,59 @@
 class Solution {
-
-    public void dfs(int r, int c, int[][] vis, int[][] grid){
-        vis[r][c]=1;
-        int m = grid.length;
-        int n = grid[0].length; 
-        int[] delrow = {-1, 0, 1, 0};
-        int[] delcol = {0, 1, 0, -1};
-
-        for(int i=0; i<4; i++){
-            int nrow = r + delrow[i];
-            int ncol = c + delcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                dfs(nrow, ncol, vis, grid);
-            } 
-        }
-    }
-
+    int m;
+    int n;
     public int numEnclaves(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int[][] vis = new int[m][n];
+        m = grid.length;
+        n = grid[0].length;
 
-        for(int j=0; j<n; j++){
-            // first row
-            if(vis[0][j]==0 && grid[0][j]==1){
-                dfs(0, j, vis, grid);
-            }
-            // last row
-            if(vis[m-1][j]==0 && grid[m-1][j]==1){
-                dfs(m-1, j, vis, grid);
-            }
+        for(int i = 0 ; i < n ; i++){
+            if(grid[0][i] == 1)
+                dfs(grid, 0, i);
         }
 
-        for(int i=0; i<m; i++){
-            // first col
-            if(vis[i][0]==0 && grid[i][0]==1){
-                dfs(i, 0, vis, grid);
-            }
-            // last col
-            if(vis[i][n-1]==0 && grid[i][n-1]==1){
-                dfs(i, n-1, vis, grid);
-            }
+        for(int i = 0 ; i < n; i++){
+            if(grid[m-1][i] == 1)
+                dfs(grid, m-1, i);
+        }
+        
+        for(int i = 0 ; i < m ; i++){
+            if(grid[i][0] == 1)
+                dfs(grid, i, 0);
         }
 
-        int cnt =0;
-
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(vis[i][j]==0 && grid[i][j]==1) cnt++;
-            }
+        for(int i = 0 ; i < m ; i++){
+            if(grid[i][n-1] == 1)
+                dfs(grid, i, n-1);
         }
 
-        return cnt;
+        int count = 0;
+        for(int i = 0 ; i < m;i++){
+            for(int j = 0 ; j < n ; j++){
+                count += grid[i][j];
+                //only unreachable 1's are left.
+            }
+        }
+        return count;
     }
+    
+
+    private void dfs(int[][] grid, int x, int y) {
+        // Out of bound
+        if (x < 0 || x >= m || y < 0 || y >= n) {
+            return;
+        }
+        // Not a land or visited
+        if (grid[x][y] == 0) {
+            return;
+        }
+        grid[x][y] = 0;
+
+        //check all boundaries;
+        dfs(grid, x+1, y);//down
+        dfs(grid, x-1, y);//up
+        dfs(grid, x, y+1);//right
+        dfs(grid, x, y-1);//left
+
+    }
+    
+    
 }
