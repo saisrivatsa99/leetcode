fd97d9a2bd0184dcbae2b0e34530e81d0bc23ffe
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-11-06 09:58:31 -0500

    Time: 2 ms (97.05%) | Memory: 41.4 MB (77.24%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
-        int l = s.length();
-        if(l==1) return false;
-        for(char ch : s.toCharArray()){
-            
-            if(ch =='(' || ch=='[' || ch=='{'){
-                st.push(ch);
+        char[] str = s.toCharArray();
+        for(int i=0; i<str.length; i++){
+            if(str[i] == '(' || str[i] =='[' || str[i]=='{'){
+                st.push(str[i]);
             }
             else{
                 if(st.isEmpty()) return false;
-                char top = st.peek();
-                if(ch==')' && top=='(') st.pop();
-                else if(ch==']' && top=='[') st.pop();
-                else if(ch=='}' && top=='{') st.pop();
-                else return false;
+                else if(str[i]==')' && st.peek()=='('){
+                    st.pop();
+                } else if(str[i]==']' && st.peek()=='['){
+                    st.pop();
+                } else if(str[i]=='}' && st.peek()=='{'){
+                    st.pop();
+                } else{
+                    return false;
+                }
             }
         }
-        if(!st.isEmpty()) return false;
-        return true;
+        if(st.isEmpty()) return true;
+        return false;
     }
 }
