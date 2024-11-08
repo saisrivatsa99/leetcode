3f89f7987fbf603de49dde6731b10d4bd16a9bd1
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-11-08 15:49:05 -0500

    Time: 0 ms (100.00%) | Memory: 40 MB (94.64%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,14 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
+        /*
+        Mathematical method:
+        in a m*n grid, we have to make m-1 moves down and n-1 moves right to reach arr[m-1][n-1]
+        This means we have a total of m+n-2 moves to make. 
+        The formula for this is:
+        (m+n-2)! / (m-1)!* (n-1)!
+
+        */
+        /*
         if(m == 1 || n == 1)
             return 1;
         m--;
@@ -14,8 +23,34 @@ public class Solution {
         for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
             res *= i;
             res /= j;
+            System.out.println(res);
         }
             
         return (int)res;
+        */
+
+        /*
+        DP method memoization:
+        We assume we have answer for ways to reach the last second step and add to it. We have a base case at 0,0 
+        where no of ways is 1.
+        */
+
+        int[][] dp = new int[m][n];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(m-1, n-1,dp);
     }
+
+    public int helper(int m, int n, int[][]dp){
+        if(m==0 && n==0){
+            return 1;
+        }
+        if(m<0 || n<0) return 0;
+        if(dp[m][n]!=-1) return dp[m][n];
+        int up = helper(m-1, n, dp);
+        int left = helper(m, n-1, dp);
+
+        return dp[m][n] = up+left;
+    } 
 }
