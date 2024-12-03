24e3835043e8c27e35cd7e26a55d1fbbfe5c6887
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2024-12-03 10:25:35 -0500

    Time: 5 ms (54.00%) | Memory: 43 MB (81.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        /*
         if(s.length()!= t.length()) return false;
         
         Map<Character, Integer> m= new HashMap();
@@ -12,6 +13,17 @@ class Solution {
         for(Character k : m.keySet()){
             if(m.get(k)!=0) return false;
         } 
+        return true;
+        */
+        if(s.length()!= t.length()) return false;
+        int[] count = new int[26];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i=0; i<26; i++){
+            if(count[i]!=0) return false;
+        }
         return true;
     }
 }
