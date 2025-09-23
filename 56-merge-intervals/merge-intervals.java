fc17dd07d409aa9b77335af5d7ca0de453fb744a
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2025-09-22 20:05:32 -0400

    Time: 8 ms (84.86%) | Memory: 46.8 MB (40.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals == null || intervals.length == 0) return intervals;
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
 
-        LinkedList<int[]> mergedIntervals = new LinkedList<>();
-        for(int[] curr: intervals){
-            if(mergedIntervals.isEmpty() || mergedIntervals.getLast()[1]< curr[0] )
-                mergedIntervals.add(curr);
-            else{
-                mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1], curr[1]);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] interval = intervals[i];
+            if (interval[0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], interval[1]);
+            } else {
+                merged.add(prev);
+                prev = interval;
             }
         }
 
-        return mergedIntervals.toArray(new int[0][]);
+        merged.add(prev);
+
+        return merged.toArray(new int[merged.size()][]);         
     }
 }
