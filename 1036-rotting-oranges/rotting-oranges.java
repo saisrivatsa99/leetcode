4b706109171feb438300ac0ffd94003f0d85175b
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2025-10-12 21:21:18 -0400

    Time: 1 ms (100.00%) | Memory: 42.5 MB (50.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,39 +2,40 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        Queue<int[]> rotten=new LinkedList<>();
-        int freshCount =0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int fc =0;
+        Queue<int[]> rotten = new LinkedList<>();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j]==2){
-                    rotten.offer(new int[]{i,j});
+                    rotten.offer(new int[] {i,j});
                 }
-                if(grid[i][j]==1)freshCount++;
+                if(grid[i][j]==1) fc++;
             }
         }
-        int rottenTime=0;
-        int madeRotten=0;
-        int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        while(!rotten.isEmpty()&& freshCount>0){
+        int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        int rottenTime=0;
+        int madeRotten =0;
+
+        while(!rotten.isEmpty() && fc>0){
             rottenTime++;
             int queueSize = rotten.size();
-            for(int i=0;i<queueSize;i++){
+            for(int i=0; i<queueSize; i++){
                 int[] curr = rotten.poll();
-                
-                int x=curr[0];
+                int x= curr[0];
                 int y = curr[1];
-                for(int j=0;j<4;j++){
-                    int nx=x+dir[j][0];
-                    int ny=y+dir[j][1];
+                for(int j=0;j<4; j++){
+                    int nx = x + dir[j][0];
+                    int ny = y + dir[j][1];
                     if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==1){
                         grid[nx][ny]=2;
                         rotten.offer(new int[]{nx,ny});
-                        freshCount--;
+                        fc--;
                     }
                 }
             }
         }
-        return freshCount==0 ? rottenTime:-1 ;
+        return fc==0? rottenTime :-1;
     }
 }
