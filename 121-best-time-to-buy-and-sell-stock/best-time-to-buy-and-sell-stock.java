f0840cca0921aac5d5620378fc757ef00c001543
saisrivatsa99 <41355682+saisrivatsa99@users.noreply.github.com>
2026-08-26 07:40:12 -0400

    Time: 1 ms (99.96%) | Memory: 94.7 MB (15.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -6,7 +6,7 @@ class Solution {
         for(int i=0; i<n; i++){
             min = Math.min(min, prices[i]);
             profit = Math.max(profit, prices[i]-min);
-        } 
+        }
         return profit;
     }
 }
